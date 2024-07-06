c32407a3e783b91da5f65e102b0cdaa8a4783d8c
Jason Godesky <jefgodesky@gmail.com>
2024-07-06 15:02:23 -0400

    World slugs need to be unique, but names don't

--- models/worldModel.go
+++ models/worldModel.go
@@ -7,7 +7,7 @@ import (
 type World struct {
 	gorm.Model
 	Slug     string `gorm:"unique" json:"slug"`
-	Name     string `gorm:"unique" json:"name"`
+	Name     string `json:"name"`
 	Creators []User `gorm:"many2many:world_creators;" json:"creators"`
 	Public   bool   `json:"active" json:"public"`
 }
