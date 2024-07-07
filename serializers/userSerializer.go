d13190a8b157481d30d77c766a345de476d1d9aa
Jason Godesky <jefgodesky@gmail.com>
2024-07-07 15:05:56 -0400

    GET /v1/users
    Index now returns stubs, and individual item GET includes stubs of the user's PC's.

--- serializers/userSerializer.go
+++ serializers/userSerializer.go
@@ -1,23 +1,40 @@
 package serializers
 
 import (
+	"github.com/jefgodesky/rnrapi/initializers"
 	"github.com/jefgodesky/rnrapi/models"
 )
 
 type SerializedUser struct {
-	Username string  `json:"username"`
-	Name     string  `json:"name"`
-	Bio      string  `json:"bio"`
-	APIKey   *string `json:"api_key,omitempty"`
-	Active   bool    `json:"active"`
+	Username   string          `json:"username"`
+	Name       string          `json:"name"`
+	Bio        string          `json:"bio"`
+	Characters []CharacterStub `json:"characters"`
+	APIKey     *string         `json:"api_key,omitempty"`
+	Active     bool            `json:"active"`
+}
+
+type UserStub struct {
+	Username string `json:"username"`
+	Name     string `json:"name"`
+	Path     string `json:"path"`
 }
 
 func SerializeUser(user models.User, apiKey *string) SerializedUser {
+	var pcs []models.Character
+	initializers.DB.Where("player_id = ? AND pc = ? AND public = ?", user.ID, true, true).Find(&pcs)
+
+	characters := make([]CharacterStub, len(pcs))
+	for i, pc := range pcs {
+		characters[i] = StubCharacter(pc)
+	}
+
 	serializedUser := SerializedUser{
-		Username: user.Username,
-		Name:     user.Name,
-		Bio:      user.Bio,
-		Active:   user.Active,
+		Username:   user.Username,
+		Name:       user.Name,
+		Bio:        user.Bio,
+		Characters: characters,
+		Active:     user.Active,
 	}
 
 	if apiKey != nil {
@@ -27,11 +44,18 @@ func SerializeUser(user models.User, apiKey *string) SerializedUser {
 	return serializedUser
 }
 
-func SerializeUsers(users []models.User) []SerializedUser {
-	serializedUsers := make([]SerializedUser, 0)
-	for _, user := range users {
-		serializedUser := SerializeUser(user, nil)
-		serializedUsers = append(serializedUsers, serializedUser)
+func StubUser(user models.User) UserStub {
+	return UserStub{
+		Username: user.Username,
+		Name:     user.Name,
+		Path:     "/users/" + user.Username,
 	}
-	return serializedUsers
+}
+
+func SerializeUsers(users []models.User) []UserStub {
+	stubs := make([]UserStub, 0)
+	for _, user := range users {
+		stubs = append(stubs, StubUser(user))
+	}
+	return stubs
 }
