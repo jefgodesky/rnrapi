d0f2171501a723b15f712efcc3b4f2e2c0630ca1
Jason Godesky <jefgodesky@gmail.com>
2024-07-08 16:49:35 -0400

    Refactoring rolling methods

--- helpers/rolls.go
+++ helpers/rolls.go
@@ -8,12 +8,12 @@ import (
 	"strings"
 )
 
-func LogRoll(roll *models.Roll, res dice.RollResult, modifier string) {
+func LogRoll(roll *models.Roll, res dice.RollResult) {
 	prefix := models.RollLogSeparator
 	if roll.Log == "" {
 		prefix = ""
 	}
-	roll.Log += prefix + res.String() + " Modifier: " + modifier
+	roll.Log += prefix + res.String()
 }
 
 func AddToResults(roll *models.Roll, results []string) {
@@ -24,7 +24,7 @@ func AddToResults(roll *models.Roll, results []string) {
 	roll.Results += prefix + strings.Join(results, models.RollResultSeparator)
 }
 
-func EvaluateFormula(formula string, roll *models.Roll) string {
+func evaluateFormulaDice(formula string, roll *models.Roll) string {
 	re := regexp.MustCompile(`\{([^}]+)\}`)
 	matches := re.FindAllStringSubmatch(formula, -1)
 	if matches == nil {
@@ -35,41 +35,41 @@ func EvaluateFormula(formula string, roll *models.Roll) string {
 		if len(match) < 2 {
 			continue
 		}
+
 		res, _, _ := dice.Roll(match[1])
-		LogRoll(roll, res, "+0")
-		total := res.Int()
-		formula = strings.Replace(formula, match[0], strconv.Itoa(total), 1)
+		LogRoll(roll, res)
+		formula = strings.Replace(formula, match[0], strconv.Itoa(res.Int()), 1)
 	}
 
 	return formula
 }
 
-func ProcessRow(row models.TableRow, roll *models.Roll) string {
-	str := row.Text
-	if row.Formula != nil {
-		str = EvaluateFormula(*row.Formula, roll)
-	}
-	return str
+func EvaluateFormula(formula string, roll *models.Roll) {
+	formula = evaluateFormulaDice(formula, roll)
+	AddToResults(roll, []string{formula})
 }
 
-func CheckTable(table models.Table, number int, roll *models.Roll) []string {
-	results := make([]string, 0)
+func ProcessRow(row models.TableRow, roll *models.Roll) {
+	if row.Formula != nil {
+		EvaluateFormula(*row.Formula, roll)
+	} else {
+		AddToResults(roll, []string{row.Text})
+	}
+}
+
+func CheckTable(table models.Table, number int, roll *models.Roll) {
 	for _, row := range table.Rows {
 		moreThanMin := row.Min == nil || number >= *row.Min
 		lessThanMax := row.Max == nil || number <= *row.Max
 		if (table.Cumulative && moreThanMin) || (moreThanMin && lessThanMax) {
-			results = append(results, ProcessRow(row, roll))
+			ProcessRow(row, roll)
 		}
 	}
-	return results
 }
 
 func RollOnTable(table models.Table, roll *models.Roll, modifier int) {
 	modifierStr := "+" + strconv.Itoa(modifier)
 	res, _, _ := dice.Roll(table.Formula + modifierStr)
-	LogRoll(roll, res, modifierStr)
-
-	total := res.Int()
-	results := CheckTable(table, total, roll)
-	AddToResults(roll, results)
+	LogRoll(roll, res)
+	CheckTable(table, res.Int(), roll)
 }
