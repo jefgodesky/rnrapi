3c268efc63ac6066c1f29d72f3127a3f75fa7313
Jason Godesky <jefgodesky@gmail.com>
2024-07-08 17:49:38 -0400

    POST /v1/rolls — Call tables recursively
    Level 3: Tables can call tables.

--- helpers/rolls.go
+++ helpers/rolls.go
@@ -1,19 +1,52 @@
 package helpers
 
 import (
+	"fmt"
+	"github.com/jefgodesky/rnrapi/initializers"
 	"github.com/jefgodesky/rnrapi/models"
 	"github.com/justinian/dice"
+	"gorm.io/gorm/clause"
 	"regexp"
 	"strconv"
 	"strings"
 )
 
-func LogRoll(roll *models.Roll, res dice.RollResult) {
+func PrepareSubRoll(original *models.Roll, table *models.Table) models.Roll {
+	sub := models.Roll{
+		Note:     original.Note,
+		Table:    *table,
+		Modifier: 0,
+	}
+
+	if original.Roller != nil {
+		sub.Roller = original.Roller
+	}
+
+	if original.Character != nil {
+		sub.Character = original.Character
+	}
+
+	if original.Ability != nil {
+		sub.Ability = original.Ability
+	}
+
+	if original.Campaign != nil {
+		sub.Campaign = original.Campaign
+	}
+
+	return sub
+}
+
+func AddToLog(roll *models.Roll, logs []string) {
 	prefix := models.RollLogSeparator
 	if roll.Log == "" {
 		prefix = ""
 	}
-	roll.Log += prefix + res.String()
+	roll.Log += prefix + strings.Join(logs, models.RollLogSeparator)
+}
+
+func LogRoll(roll *models.Roll, res dice.RollResult) {
+	AddToLog(roll, []string{res.String()})
 }
 
 func AddToResults(roll *models.Roll, results []string) {
@@ -44,8 +77,42 @@ func evaluateFormulaDice(formula string, roll *models.Roll) string {
 	return formula
 }
 
+func evaluateFormulaTables(formula string, roll *models.Roll) string {
+	re := regexp.MustCompile(`\[([^\]]+)\]`)
+	matches := re.FindAllStringSubmatch(formula, -1)
+	if matches == nil {
+		return formula
+	}
+
+	for _, match := range matches {
+		if len(match) < 2 {
+			continue
+		}
+
+		tableSlug := match[1]
+		var table models.Table
+		result := initializers.DB.
+			Preload(clause.Associations).
+			Where("slug = ?", tableSlug).
+			First(&table)
+		if result.Error != nil {
+			continue
+		}
+
+		subRoll := PrepareSubRoll(roll, &table)
+		RollOnTable(table, &subRoll, 0)
+		results := strings.Split(subRoll.Results, models.RollResultSeparator)
+		resultsCS := strings.Join(results, ", ")
+		formula = strings.Replace(formula, match[0], resultsCS, 1)
+		AddToLog(roll, []string{subRoll.Log})
+	}
+
+	return formula
+}
+
 func EvaluateFormula(formula string, roll *models.Roll) {
 	formula = evaluateFormulaDice(formula, roll)
+	formula = evaluateFormulaTables(formula, roll)
 	AddToResults(roll, []string{formula})
 }
 
@@ -68,6 +135,9 @@ func CheckTable(table models.Table, number int, roll *models.Roll) {
 }
 
 func RollOnTable(table models.Table, roll *models.Roll, modifier int) {
+	logMsg := fmt.Sprintf("Rolling on %s [%s]", table.Name, table.Slug)
+	AddToLog(roll, []string{logMsg})
+
 	modifierStr := "+" + strconv.Itoa(modifier)
 	res, _, _ := dice.Roll(table.Formula + modifierStr)
 	LogRoll(roll, res)
